a817c5d1b0c9b1e11816f2224a39ada57d5c5766
Ludmila Mangaziejew <ludmilamangaziejew@macbook-air-ludmila.home>
2023-08-26 00:07:07 +0200

    first page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default function RootLayout({children}) {
-    return (
-        <html lang="en">
-            <body>
-                {children}
-            </body>
-        </html>
-    )
+export default function RootLayout({ children, header, footer }) {
+  return (
+    <html lang="en">
+      <body>
+        <header>{header}</header>
+        <main>{children}</main>
+        <footer>{footer}</footer>
+      </body>
+    </html>
+  );
 }
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <React.Fragment>
+        <h1>Indie Gamer</h1>
+        <p>only the best indie games, reviewed for you.</p>
+    </React.Fragment>
+  );
+}
